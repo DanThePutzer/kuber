4cab67fcf3f8751775e04004605a4789fe03239a
Daniel Putzer <danielputzer.work@gmail.com>
2020-07-24 12:34:23 +0200

    feat: Add upload_docker script

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=ml .
+docker build --tag=kuber .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image list
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 ml
+docker run -p 8000:80 kuber
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=danceladus/kuber
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=danceladus
+docker tag kuber $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
